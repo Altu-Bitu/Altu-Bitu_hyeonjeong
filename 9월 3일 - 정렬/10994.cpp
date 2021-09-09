b5574d741823e4ed38624298af35c89919b662e9
hyeonjeong <py0429@ewhain.net>
2021-09-09 22:55:10 +0900

    [정렬] 9월 9일 - update

--- 9월 3일 - 정렬/10994.cpp
+++ 9월 3일 - 정렬/10994.cpp
@@ -4,34 +4,41 @@
 using namespace std;
 
 vector<vector<char> > a;
+
+void drawStar(int n, int row, int col) {//별그려주는 함수
+	for (int i = col; i < col + n; i++) {//첫째줄,마지막줄 채우기
+		a[row][i] = '*'; //첫줄
+		a[row + (n - 1)][i] = '*'; //마지막 줄
+	}
+
+	for (int i = row + 1; i < row + (n - 1); i++) {//행 바뀌면서 옆줄 채우기
+		a[i][col] = '*';
+		a[i][col + (n - 1)] = '*';
+	}
+}
+
+void starArray(int n, int row, int col) {
+	drawStar(n, row, col);//별그려주기
+	if (n == 1) {
+		return;
+	}
+	starArray(n - 4, row + 2, col + 2); //재귀함수
+}
+
 int main() {
 	int n, star;
 	int i, j;
 	cin >> star;
-	n = 1 + 4 * (star - 1);
-	a.assign(n, vector<char>(n, ' '));
+	n = 1 + 4 * (star - 1); //별개수
+	a.assign(n, vector<char>(n, ' ')); //배열에 공백넣어주기
 
+	starArray(n, 0, 0);
 
-	for (i = 0; i < n / 2 + 1; i += 2) {
-		for (j = i; j < n - i; j++) {
-			a[i][j] = '*';
-			a[n - (i + 1)][j] = '*';
-
-		}
-
-	}
-	for (i = 0; i < n / 2 + 1; i += 2) {
-		for (j = i + 1; j < n - (i + 1); j++) {
-			a[j][i] = '*'; 
-			a[j][n - (i + 1)] = '*'; 
-		}
-	}
-
-
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < n; i++) { //출력
 		for (j = 0; j < n; j++)
 			cout << a[i][j];
 		cout << endl;
 	}
+
 	return 0;
 }
--- 9월 3일 - 정렬/1316.cpp
+++ 9월 3일 - 정렬/1316.cpp
@@ -5,49 +5,41 @@
 using namespace std;
 
 vector<string> str1;
-vector<int> ct;
 
-//sort해서 비교 or visited한거 true로 설정
+int cnt = 0;
+
+void wordchecker(string str) {
+	int visited[26] = { 0 };
+	for (int i = 0; i < str.length(); i++) {
+		if (i == 0) {
+			visited[(int)str[i] - 97] = 1; //알파벳 순서에 따라 방문 표시
+			continue;
+		}
+
+		if (visited[(int)str[i] - 97] == 1 && str[i - 1] != str[i]) //방문한 경우와 연속된 문자열 아닌경우
+			return;
+
+		visited[(int)str[i] - 97] = 1;
+	}
+	cnt++;
+}
+
+
+//visited한거 true로 설정
 int main() {
 	int n, result = 0;
 
+
 	cin >> n;
 	str1.assign(n, "");
-	ct.assign(n, 0);
+
 	//입력
 	for (int i = 0; i < n; i++) {
 		cin >> str1[i];
+		wordchecker(str1[i]);
 	}
 
-	//반복되는 단어 count x
-	for (int i = 0; i < n; i++) {
-		int j = 1, cnt = 1;
-		while (str1[i][j]) {
-			if (str1[i][j] != str1[i][j - 1])
-				cnt++;
-			j++;
-		}
-		ct[i] = cnt;
-	}
-
-	//정렬
-	for (int i = 0; i < n; i++) {
-		sort(str1[i].begin(), str1[i].end());
-	}
-
-	//정렬한것과 정렬 전 count 개수 비교
-	for (int i = 0; i < n; i++) {
-		int j = 1, cnt = 1;
-		while (str1[i][j]) {
-			if (str1[i][j] != str1[i][j - 1])
-				cnt++;
-			j++;
-		}
-		if (ct[i] == cnt)
-			result++;
-	}
-
-	cout << result;
+	cout << cnt;
 
 	return 0;
 }
