0970e823e5faa26ee0bd20b4af014d7b0aa283eb
hyeonjeong Park <py0429@ewhain.net>
2021-09-23 13:07:30 +0900

    [정수론 및 조합론] 9월 23일 - update
    6588 시간초과

--- 9월 17일 - 정수론 및 조합론/6588.cpp
+++ 9월 17일 - 정수론 및 조합론/6588.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<cmath>
 using namespace std;
 
 vector<bool> num;
@@ -7,14 +8,13 @@ const int MAX = 1000001; //미리 제거하기 위해 max선언
 
 void isPrime() {
 	num[0] = num[1] = false;
-	for (int i = 2; i < sqrt(MAX); i++) {
+	for (int i = 2; i <= sqrt(MAX-1); i++) {
 		if (num[i]) {
-			for (int j = i * i; i < MAX; j += i) //제곱한것들은 이미 소수 아니므로
+			for (int j = i * i; j < MAX; j += i) //제곱한것들은 이미 소수 아니므로
 				num[j] = false;
 		}
 	}
 }
-
 
 
 int main() {
@@ -22,7 +22,8 @@ int main() {
 	cin.tie(NULL);
 
 	int n=1,a,b;
-	bool test=false;
+	
+
 	num.assign(MAX, true);
 	isPrime(); //소수인지 판단
 
@@ -30,17 +31,17 @@ int main() {
 		cin >> n;
 		if (n == 0)
 			break;
-		
-		for (int i = 3; i < n; i++) {
+		bool test = false;
+		for (int i = 3; i < n; i+=2) {
 			a = i;
 			b = n - i;
-			if (num[a] && num[b]) {
-				cout << n << " = " << a << " + " << b<<endl ;
+			if (num[a] && num[b]) { //둘다 소수라면
+				cout << n << " = " << a << " + " << b<<endl;
 				test = true;
 				break;
 			}
 		}
-			if(test==0)
+			if(!test)
 				cout << "Goldbach's conjecture is wrong."<<endl;
 		}
 	
