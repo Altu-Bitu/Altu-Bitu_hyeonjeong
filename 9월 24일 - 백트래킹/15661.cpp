270f49174e78d166f790af9e27903738e7030c4f
hyeonjeong Park <py0429@ewhain.net>
2021-10-01 10:36:14 +0900

    [백트래킹] 10월 1일 - 추가제출

--- 9월 24일 - 백트래킹/15661.cpp
+++ 9월 24일 - 백트래킹/15661.cpp
@@ -1,60 +1,67 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-vector<vector<int> > ability;
-vector<int> rank;
-void abilitySum(int a,int b) {
-	vector<bool> check;//체크할 변수 만들기
-	check.assign(a+b, 0);
-	int sum = 0;
-	int num = 0;
-	for (int i = 0; i <= b; i++) {
-		check[i] = 1;
-		for (int j = 1; j < a; j++) {
-			if (i == j)
-				continue;
+const int SIZE = 20;
+const int INF = 10e8;
 
-			while (num < a-1) {
-				if (check[j] == 0) {
-					check[j] = 1;
-				}
-				num++;
-			}
-		}
-	}
-		
+int n;
+int answer = INF;//능력치 최소값
+int power[SIZE][SIZE]; //능력치 값
+bool is_start[SIZE];//스타트,링크 인원수 체크
+int start[SIZE], link[SIZE]; //스타트와 링크 팀에 속하는 사람
+
+//각 팀의 능력치를 계산하는 함수
+int calcPower(int arr[], int size) {//팀배열, 팀인원수
+    int result = 0;
+
+    //모든 Sij의 합
+    for (int i = 0; i < size; i++) {//팀인원수만큼 능력치 더하기
+        for (int j = i + 1; j < size; j++)
+            result += power[arr[i]][arr[j]] + power[arr[j]][arr[i]];//인덱스 ij인 경우와 ji인 경우 능력치 값에 더하기
+    }
+    return result;
 }
 
-void abilityRec(int a, int b) {//(스타트, 링크)
-	if (b <= (a+b)/2 )//탈출조건
-		return;
-	else {
-		abilitySum(a, b);
-		abilityRec(a + 1, b - 1);
-	}
-
+void backtracking(int cnt, int st) {
+    if (cnt == n) //팀인원 채운경우(기저조건)
+        return;
+    if (cnt > 0) { //최소 1명이 팀에 있어야 함
+        int idx = 0;//인덱스
+        for (int i = 0; i < n; i++) {//팀 배정하기
+            if (!is_start[i]) //스타트 팀이 아니라면->링크팀(스타트 팀으로 꾸려나가기)
+                link[idx++] = i;//링크팀에 인원추가
+        }
+        int diff_value = abs(calcPower(link, idx) - calcPower(start, cnt)); //두 팀의 능력치 차이(절대값으로 음수인경우도 양수로)
+        answer = min(answer, diff_value); //차이의 최솟값 저장
+    }
+    for (int i = st; i < n; i++) {
+        is_start[i] = true; //링크 팀에 속하는 사람 추리기 위해 스타트 팀원 체크
+        start[cnt] = i; //스타트 팀에 배치
+        backtracking(cnt + 1, i + 1);//백트래킹(재귀)
+        if (i == 0) //1번 사람이 링크 팀이 됨 -> 어차피 스타트 팀이었던 경우와 값 같음
+            return;// 스타트 팀이었던 구성이 링크 팀 된 경우도 차이값은 같기 때문에
+        is_start[i] = false;//다시 스타트 팀 초기화
+    }
 }
+
+
 
 int main() {
-	cin.tie(NULL);
-	ios::sync_with_stdio(false);
-	int n, input;
-	cin >> n; 
-	ability.assign(n, vector<int>(n, 0)); //n*n 이차원 배열 선언
+    //입력
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> power[i][j]; //능력치 입력받기
+        }
+    }
 
-	for (int i = 0; i < n; i++) { //능력치 입력받기
-		for (int j = 0; j < n; j++) {
-			cin >> input;
-			ability[i][j] = input;
-		}
-	}
-	
-	abilityRec(2, n - 2); //팀 나누기(스타트팀, 링크팀)
+    //연산
+    backtracking(0, 0);
 
+    //출력
+    cout << answer << '\n';
 
-
-
-	return 0;
+    return 0;
 }
--- 9월 24일 - 백트래킹/15665.cpp
+++ 9월 24일 - 백트래킹/15665.cpp
@@ -3,32 +3,32 @@
 #include<algorithm>
 using namespace std;
 
-const int SIZE = 7;
+const int SIZE = 7;//배열 크기
 set<int> num;//중복되는것 제거하면서 저장
 int seq[SIZE]; //수열 저장
-int n, m;
+int n, m;//전역변수로 
 void backtracking(int cnt) { //중복 허용하여 수열 뽑기
-	if (cnt == m) {//기저조건
+	if (cnt == m) {//기저 조건
 		for (int i = 0; i < m; i++)
-			cout << seq[i] << ' ';
+			cout << seq[i] << ' '; //출력하면서 끝내기
 		cout << '\n';
-		return;
+		return;//출력 끝내고 횟수 남아있으면 수열 출력
 	}
 	for (auto iter = num.begin(); iter != num.end(); iter++) {
-		seq[cnt]= *iter;
-		sequNumber(cnt + 1); //재귀로 중복허용
+		seq[cnt]= *iter; //num에서 저장된것 수열배열에 저장하기
+		backtracking(cnt + 1); //재귀로 중복허용(중복허용하며 m개의 자연수 출력)
 	}
 }
 
 int main() {
-	int input;
-	cin >> n >> m;
+	int input;//입력 받으며 저장할 것
+	cin >> n >> m;// n개의 자연수, 자연수 m
 	for (int i = 0; i < n; i++) {
-		cin >> input;
-		num.insert(input);
+		cin >> input;//입력 받기
+		num.insert(input);//set함수로 입력받고 저장하면서 중복되는 수는 제거하기
 	}
 
 
-	backtracking(0);
+	backtracking(0);//백트래킹
 	return 0;
 }
